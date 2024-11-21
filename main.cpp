ec3672d0a53efc2e933eaa2cc8c811340ee1ea6d
Maksim <aheon00@mail.ru>
2024-11-22 00:47:17 +0500

    added second and sum

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 int main(){
 
 	std::cout << " Hello world ! " << std::endl;
+	std::cout << " And again.. again... " << std::endl;
+	int a =5;
+	int b = 6;
+	std::cout << a + b << std::endl;
 	return 0;
 }
